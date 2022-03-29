1134cde314ef73cd3dce3e132e7de873f3f3b33e
JosefCurda <226380@vutbr.cz>
2022-03-29 09:03:59 +0200

    sdvb

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -5,12 +5,19 @@ def is_whol_number(number):
     return False
 
 
-s=0
+def is_even_number(number):
+    if number % 2 == 1:
+        return True
+    else:
+        return False
+
 
 def main():
     my_number = 5.4
     get_is_whole_number = is_whol_number(my_number)
     print(get_is_whole_number)
+    a = is_even_number(my_number)
+    print(a)
 
 if __name__ == '__main__':
     main()
