dad2a3471d9e411231284039e727b235e2e50da8
JosefCurda <226380@vutbr.cz>
2022-03-29 08:54:45 +0200

    supér

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,5 +1,5 @@
-def is_whole_number(number):
-    if number % 1 ==0:
+def is_whol_number(number):
+    if (number % 1 ==0) and (number > 0):
         return True
 
     return False
@@ -9,7 +9,7 @@ s=0
 
 def main():
     my_number = 5.4
-    get_is_whole_number = is_whole_number(my_number)
+    get_is_whole_number = is_whol_number(my_number)
     print(get_is_whole_number)
 
 if __name__ == '__main__':
